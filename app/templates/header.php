2107c7fc1912ba0bff0bbe28bd15c81dc2a43bfa
ilyasam357 <ilyasam357@gmail.com>
2022-04-08 10:14:06 +0700

    add: layout transsactions

--- app/templates/header.php
+++ app/templates/header.php
@@ -8,6 +8,7 @@
     <title><?=$page ?? 'Dashboard'?> | Hutang Piutang App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/public/css/stlye.css">
 
