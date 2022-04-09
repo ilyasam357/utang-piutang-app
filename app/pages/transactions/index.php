9e0da5dfda9f124a8eb43e33067abd4acd8b8454
ilyasam357 <ilyasam357@gmail.com>
2022-04-09 09:51:24 +0700

    fix: chnages url to create debt transaction

--- app/pages/transactions/index.php
+++ app/pages/transactions/index.php
@@ -1,7 +1,8 @@
 <div class="container">
     <h1 class="mb-3">Transactions -<?=$where === 'debt' ? 'utang' : 'piutang'?></h1>
 
-    <a href="/app/pages/transactions/create.php" class="btn btn-primary"><i class="bi bi-plus-circle"></i> Tambah </a>
+    <a href="/app/index.php?page=transactions&action=create" class="btn btn-primary"><i class="bi bi-plus-circle"></i>
+        Tambah </a>
 
     <table class="table table-striped table-bordered mt-4">
         <thead class="table-dark">
