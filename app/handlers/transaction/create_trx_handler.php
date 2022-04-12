1354aea9a8d8c0ee2f18ef5c9bc8bce2ff965be5
ilyasam357 <ilyasam357@gmail.com>
2022-04-12 09:36:43 +0700

    add :validasi data

--- app/handlers/transaction/create_trx_handler.php
+++ app/handlers/transaction/create_trx_handler.php
@@ -2,16 +2,41 @@
 if (isset($_POST['action'])) {
     if ($_POST['action'] === "create_trx") {
         $use_for = htmlspecialchars($_POST['use_for']);
-        $fav_person = htmlspecialchars($_POST['fav_person']);
+        $fav_person = 1;
         $new_person = htmlspecialchars($_POST['new_person']);
         $nominal = htmlspecialchars($_POST['nominal']);
-        $transaction_at = htmlspecialchars($_POST['transaction_at']);
-        $due_date = htmlspecialchars($_POST['due_date']);
+        $transaction_at = "2022-04-07 09:47:38";
+        $due_date = "2022-04-08 09:47:38";
         $type = htmlspecialchars($_POST['type']);
 
-        echo "<pre>" . print_r([
-            "create_trx_handler.php - 3",
-            $_POST,
-        ], 1) . "</pre>";
+        $errors = [];
+
+        foreach ($_POST as $key => $val) {
+            if (empty($val)) {
+                $new_key = ucfirst($key);
+
+                array_push($errors, str_replace("_", " ", $new_key) . "kosong");
+            }
+        }
+
+        if (!empty($nominal) && strlen($nominal) > 11) {
+            array_push($errors, "gak boleh dari sebelas");
+        }
+
+        if (empty($errors)) {
+
+            $insert = mysqli_query($con, "INSERT INTO  `transactions` (`type`, `use_for`, `person_id`, `nominal`, `transaction_at`, `due_date`) VALUES ('$type' ,'$use_for', '$fav_person', '$nominal', '$transaction_at', '$due_date' )");
+
+            mysqli_close($con);
+
+            if ($insert) {
+                $alert = ['success', ['Berhasil di tambah  kan  ']];
+            } else {
+                $alert = ['danger', ['Gagal di tambahkan  ']];
+            }
+
+        } else {
+            $alert = ['danger', $errors];
+        }
     }
 }
--- app/pages/transactions/create.php
+++ app/pages/transactions/create.php
@@ -3,14 +3,34 @@
 
     <div class="row">
         <div class="col-6 mb-4">
+            <?php
+if (isset($alert)):
+?>
+            <div class="alert alert-<?=$alert[0]?> alert-dismissible fade show" role="alert">
+                <ul>
+                    <?php
+// echo "<pre>". print_r([
+//     "register.php - 63",
+//     $alert
+// ], 1) ."</pre>";
+foreach ($alert[1] as $alert_msg) {
+    echo '<li><strong>' . $alert_msg . '</strong></li>';
+}
+?>
+                </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" arial-label="Close"></button>
+            </div>
+            <?php
+endif;
+?>
             <form action=" <?php $_SERVER['REQUEST_URI']?>" method="POST">
                 <div class="mb-3">
                     <label for="use_for" class="form-label">Kegunaan</label>
-                    <input type="text" class="form-control" id="use_for" name="use_for" value="">
+                    <input type="text" class="form-control" id="use_for" name="use_for" value="<?=$use_for ?? ''?>">
                 </div>
                 <div class="mb-3">
                     <label for="fav_person" class="form-label">Orang Favorit</label>
-                    <select class="form-select" name="fav_person  ">
+                    <select class="form-select" name="fav_person">
                         <option selected value="-">Pilih orang favorit</option>
                         <option value="budi">Budi </option>
                         <option value="joko">Joko</option>
@@ -20,11 +40,12 @@
                 </div>
                 <div class="mb-3">
                     <label for="new_person" class="form-label">Orang Baru</label>
-                    <input type="text" class="form-control" id="new_person" name="new_person" value="">
+                    <input type="text" class="form-control" id="new_person" name="new_person"
+                        value="<?=$new_person ?? ''?>">
                 </div>
                 <div class="mb-3">
                     <label for="nominal" class="form-label">Nominal</label>
-                    <input type="text" class="form-control" id="nominal" name="nominal" value="">
+                    <input type="text" class="form-control" id="nominal" name="nominal" value="<?=$nominal ?? ''?>">
                 </div>
                 <div class="mb-3">
                     <label for="transaction_at" class="form-label">Waktu <?=$title?> </label>
@@ -35,7 +56,7 @@
                     <label for="due_date" class="form-label">kapan Bayar</label>
                     <input type="datetime-local" class="form-control" id="due_date" name="due_date" value="">
                 </div>
-                <input type="hidden" name="type" value="create_trx">
+                <input type="hidden" name="type" value="<?=$where?>">
                 <input type="hidden" name="action" value="create_trx">
                 <button class="btn btn-primary">Buat <?=$title?></button>
                 <a class="btn btn-danger" href="/app/index.php?page=transactions&view=<?=$where?>">Cencel</a>
