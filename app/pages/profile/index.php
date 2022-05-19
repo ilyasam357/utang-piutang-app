923deca3775663d9c0a370ce2f90ab3c367921bb
ilyasam357 <ilyasam357@gmail.com>
2022-05-19 20:10:00 +0700

    tambah

--- app/pages/profile/index.php
+++ app/pages/profile/index.php
@@ -1,1 +1,24 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="card shadow rounded border border-success  " style="width:15rem;">
+                <img src="/public/profil.png" class="rounded-circle mx-5 mt-3 "
+                    style=" width: 142px;   height: 142px;   object-fit: cover;   object-position: center; " alt="...">
 
+                <div class="card-body  bg-secondary mt-3">
+                    <h3 class="border-top  border-2 border-success  text-white">Nama lengkap</h3>
+                    <p class="fst-italic  text-white">ahmad</p>
+                    <h3 class="border-top  border-2 border-success  text-white">User Name </h3>
+                    <p class="fst-italic  text-white">ahmad123</p>
+                    <h3 class="border-top  border-2 border-success  text-white">No Wa</h3>
+                    <p class="fst-italic  text-white">098766765632</p>
+                    <h3 class="border-top  border-2 border-success  text-white">Email</h3>
+                    <p class="fst-italic  text-white">ahmad@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+
+
+    </div>
