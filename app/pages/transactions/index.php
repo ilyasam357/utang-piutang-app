3579626343287be8caf70e35d37a5e27a9219608
ilyasam357 <ilyasam357@gmail.com>
2022-06-01 08:16:32 +0700

    fix: add  details

--- app/pages/transactions/index.php
+++ app/pages/transactions/index.php
@@ -109,26 +109,69 @@ foreach ($transactions as $transaction):
             </td>
 
         </tr>
-        <div class="modal fade" id="trx_modal_<?=$transaction['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog modal-lg">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel"><?=strtoupper($transaction['type'])?>
-                            #<?=$transaction['id']?></h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <?php endforeach?>
         </tbody>
     </table>
+
+    <?php
+$num = 1;
+
+foreach ($transactions as $transaction):
+
+    // echo "<pre>" . print_r([
+    //     "index.php - 122",
+    //     $transactions,
+    // ], 1) . "</pre>";die;
+    ?>
+
+
+    <div class="modal fade" id="trx_modal_<?=$transaction['id']?>" tabindex="-1" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel"><?=strtoupper($transaction['type'])?>
+                        #<?=$transaction['id']?></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <table class="table table-striped table-bordered mt-4">
+                        <thead class="table-dark">
+                            <tr>
+                                <th>NO</th>
+                                <th>Untuk</th>
+                                <th>Orang</th>
+                                <th>Nominal</th>
+                                <th>Status</th>
+                                <th>tanggal hutang</th>
+                                <th>Jatuh Tempo</th>
+                                <th>created at</th>
+                                <th>Update at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><?=$num++?></td>
+                                <td><?=$transaction['use_for']?></td>
+                                <td><?=$transaction['name']?></td>
+                                <td><?=$transaction['nominal']?></td>
+                                <td>unpaid</td>
+                                <td><?=date("d F Y H:i:s", strtotime($transaction['transaction_at']))?></td>
+                                <td><?=date("d F Y H:i:s", strtotime($transaction['due_date']))?></td>
+                                <td><?=date("d F Y H:i:s", strtotime($transaction['created_at']))?></td>
+                                <td><?=date("d F Y H:i:s", strtotime($transaction['updated_at']))?></td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php endforeach?>
 </div>
