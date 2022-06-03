785fcbcf94a72dbd89a10f8093214b637e4d7aa8
ilyasam357 <ilyasam357@gmail.com>
2022-06-03 09:13:03 +0700

    fix: add page dasboard

--- app/pages/dashboard.php
+++ app/pages/dashboard.php
@@ -1,3 +1,66 @@
 <div class="container">
     <h1>Dashboard</h1>
-</div>
+
+    <div class="row mt-4">
+
+        <div class="col-md-3">
+            <div class="card text-dark bg-success mb-3 text-white" style=" height: 170px;">
+                <div class="card-header text-center">Jumlah Hutang</div>
+                <div class="card-body">
+                    <h1 class=" text-center"><?=$resultDebt['nominal']?></h1>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-success mb-3 text-white" style=" height: 170px;">
+                <div class="card-header text-center">Jumlah Piutang</div>
+                <div class="card-body">
+                    <h1 class=" text-center"><?=$resultReceivable['nominal']?></h1>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-success mb-3 text-white" style="height: 170px;">
+                <div class="card-header text-center">Jumlah Trx Hutang</div>
+                <div class="card-body">
+                    <h1 class=" text-center fs-4">1.000.000.00</h1>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-success mb-3 text-white" style=" height: 170px;">
+                <div class="card-header text-center">Jumlah Trx Hutang</div>
+                <div class="card-body">
+                    <h1 class=" text-center fs-4">1.000.000.00</h1>
+
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-info mb-3 text-white" style=" height: 170px;">
+                <div class="card-header">Paling Sering Hutang</div>
+                <div class="card-body">
+
+                    <ol>
+                        <li> fulan </li>
+                    </ol>
+                    <a href="" class="btn btn-link">More...</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card text-dark bg-info mb-3 text-white" style=" height: 170px;">
+                <div class="card-header">Paling Sering Hutang</div>
+                <div class="card-body">
+
+                    <ol>
+                        <li>fulan </li>
+                    </ol>
+                    <a href="" class="btn btn-link">More...</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
