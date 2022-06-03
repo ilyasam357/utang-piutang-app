0e405799bfa8a582d73745b49a75b9fd08d7f94b
ilyasam357 <ilyasam357@gmail.com>
2022-06-03 09:36:21 +0700

    fix: change temp niminla

--- app/handlers/home/home_handler.php
+++ app/handlers/home/home_handler.php
@@ -5,7 +5,7 @@ $trxType = ['debt', 'receivable'];
 foreach ($trxType as $type) {
     $amout = 'amout' . ucwords($type);
 // $$ nama nya reference variable , yaitu jadiin value untuk sebuah variable
-    $$amout = mysqli_query($con, "SELECT SUM(nominal) as nominal FROM transactions WHERE type = '$type' AND user_id=' $session_user_id';");
+    $$amout = mysqli_query($con, "SELECT SUM(nominal) - SUM(temp_nominal) as nominal FROM transactions WHERE type = '$type' AND user_id=' $session_user_id';");
 
     $result = 'result' . ucwords($type);
     $$result = mysqli_fetch_assoc($$amout);
